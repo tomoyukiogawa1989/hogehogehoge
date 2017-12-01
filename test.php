275acfd67fb100d75c1d09a97137c6f1ba485dfc
富士通\1 <n-momoko0730@outlook.jp>
2017-12-01 14:57:05 +0900

    no message

--- test.php
+++ test.php
@@ -7,13 +7,8 @@
   echo 'test';
   echo 'hogehogehogehoge';
   echo 'hogehogehogehoge';
-<<<<<<< HEAD
-  echo 'Tomtom参上！！';
-  	echo 'パンティパンティ';
-=======
   echo 'Tomtom参上！！<br>';
   echo 'tom';
   echo 'ノーパンティー<br>'
   echo 'MOMOKO参上!';
->>>>>>> 7cab5f22fb0fd95ae73245bd09c3f0bc1559dc27
 ?>
